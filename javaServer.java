8d0a19f7633547c982db7f852ba4d7add3d7bd80
Nicholas Persley <nspersley20@gmail.com>
2023-02-08 17:01:15 -0700

    Made Java server run in parallel and new java server constants
    - Server now connects to NIST (time.nist.gov:13) and relays the time information to the client

--- javaServer.java
+++ javaServer.java
@@ -4,11 +4,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.InetAddress;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class EchoServer extends Thread {
+public class EchoServer extends Thread implements javaServerConstants {
 
     private static ServerSocket serverSocket;
     private static int port;
@@ -33,19 +34,14 @@ public class EchoServer extends Thread {
 
     public void runServerLoop() throws IOException {
 
-        System.out.println("Echo server started");
+        System.out.println("Daytime server is listening on port "+LISTEN_PORT);
 
         while (true) {
-            System.out.println("Waiting for connections on port #" + port);
 
             try {
-
                 (new EchoServer(serverSocket.accept())).start();
-
             } catch (IOException exception) {
-
-                System.err.println("Error handing new client");
-
+                System.err.println("Error handing client to a new thread");
             }
         }
     }
@@ -53,62 +49,88 @@ public class EchoServer extends Thread {
     public static void main(String args[]) throws Exception {
         // create instance of echo server
         // note that hardcoding the port is bad, here we do it just for simplicity reasons
-        EchoServer echoServer = new EchoServer(23657);
+        EchoServer echoServer = new EchoServer(LISTEN_PORT);
 
         // fire up server loop
         echoServer.runServerLoop();
     }
 
-    //@Override
+    @Override
     public void run() {
 
         // Handle every client in parallel
         DataInputStream fromClient = null;
         DataOutputStream toClient = null;
+        DataInputStream fromNISTServer = null;
+        Socket socketToNIST = null;
 
-        int charFromClient = 0;
-        int state = 0;
+        int charFromNISTServer = 0;
         boolean keepGoing = true;
 
         // show that we are connected to client
-        System.out.println("Client connected ...");
+        System.out.println("\nClient connected ...");
 
-        // first get the streams
+        // Get streams to and from the NIST server (time.nist.gov)
         try {
-            fromClient = new DataInputStream(clientSocket.getInputStream());
+            // Connect to the NIST server
+            socketToNIST = connectToNISTServer();
+
+            // Set up input/output streams to/from the NIST server
             toClient = new DataOutputStream(clientSocket.getOutputStream());
+            fromNISTServer = new DataInputStream(socketToNIST.getInputStream());
         } catch (IOException e) {
-            System.err.println("Error opening network streams");
+            System.err.println("Error opening stream to NIST server");
             return;
         }
 
-        // now talk to the client
+        // Now transfer bytes given by NIST to the client
         while (keepGoing) {
             try {
-                charFromClient = fromClient.readByte();
-                System.out.print((char)charFromClient);
+                // Read the next byte given to us by NIST
+                charFromNISTServer = fromNISTServer.readByte();
+                System.out.print((char)charFromNISTServer);
             } catch (IOException e) {
-                System.err.println("Error reading character from client");
+                System.err.println("Error reading character from NIST");
                 return;
             }
 
             try {
-                toClient.writeByte(charFromClient);
+                // Send the current byte of information to the client
+                toClient.writeByte((char)charFromNISTServer);
             } catch (IOException e) {
                 System.err.println("Error writing character to client");
                 return;
             }
 
-            if (charFromClient == 'q') {
-                System.out.println("\nBailing out!");
+            if (charFromNISTServer == END_MESSAGE_CHAR) {
                 keepGoing = false;
             }
         }
 
         try {
+            socketToNIST.close();
             clientSocket.close();
+            System.out.println("\nTime information sent, booting client...");
         } catch (IOException e) {
             System.err.println("Error closing socket to client");
         }
+    }
+
+    public Socket connectToNISTServer() {
+        String NISTserverAddressString;
+        InetAddress NISTserverAddress;
+        Socket clientSocketToNIST = null;
+
+        try {
+            NISTserverAddress = InetAddress.getByName(NIST_HOST_NAME);
+            NISTserverAddressString = NISTserverAddress.getHostAddress();
+
+            // Create new client socket and connect to the server
+            clientSocketToNIST = new Socket(NISTserverAddressString, NIST_PORT);
+        } catch(IOException e) {
+            System.err.println("Error connecting to NIST server");
+        }
+
+        return clientSocketToNIST;
     }
 }
---
+++ javaServerConstants.java
@@ -0,0 +1,6 @@
+interface javaServerConstants {
+    public static String NIST_HOST_NAME = "time.nist.gov";
+    public static int NIST_PORT = 13;
+    public static char END_MESSAGE_CHAR = '*';
+    public static int LISTEN_PORT = 23657;
+}
