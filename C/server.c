dac31e3f25e44bc725349483f130a58fa040a6c4
Nicholas-Ray-NAU <93357802+Nicholas-Ray-NAU@users.noreply.github.com>
2023-02-08 20:35:51 -0700

    Update server.c
    updated server.c (daytime)

--- C/server.c
+++ C/server.c
@@ -1,6 +1,5 @@
 #include "server.h"
 
-
 /* ************************************************************************* */
 /* MAIN                                                                      */
 /* ************************************************************************* */
@@ -19,9 +18,9 @@ int main(int argc, char** argv) {
     }
     
     // name the socket (making sure the correct network byte ordering is observed)
-    server_address.sin_family      = AF_INET;           // accept IP addresses
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
-    server_address.sin_port        = htons(PORT);       // port to listen on
+    server_address.sin_family      = AF_INET;                  // accept IP addresses
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);        // accept clients on any interface
+    server_address.sin_port        = htons(LISTEN_PORT);       // port to listen on
     
     // binding unnamed socket to a particular port
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) != 0) {
@@ -46,7 +45,7 @@ int main(int argc, char** argv) {
         // note that this is a naive approach, i.e. there are race conditions
         // for now this is okay, assuming low load
         pthread_t thread;
-        if (pthread_create(&thread, NULL, handle_client, (void*)&client_socket) != 0) {
+        if (pthread_create(&thread, NULL, (void*)handle_client, (void*)&client_socket) != 0) {
             perror("Error creating thread");
             exit(EXIT_FAILURE);
         }
@@ -66,32 +65,64 @@ int main(int argc, char** argv) {
 /* ************************************************************************* */
 
 void* handle_client(void* arg) { // xxxxx
-    int client_socket = *((int*)arg);   // the socket connected to the client
-    char input;
-    int keep_going = TRUE;
+    int client_socket = *((int*)arg);         // the socket connected to the client
+	char *time_server_name = "time.nist.gov"; // hostname
+    int time_socket;                          // client side socket
+    struct sockaddr_in time_address;          // client socket naming struct
+    struct hostent *host_struct;              // resolved hostname struct
+    struct in_addr **addr_list;               // Resolved IP address(es)
+    char server_IP[100];                      // ip address buffer
+    char in_char = '\0';                      // input buffer
+    char output_time[100];                    // buffer for output
+    int index;
     
-    while (keep_going) {
-        // read char from client
-        switch (read(client_socket, &input, sizeof(char))) {
-            case 0:
-                keep_going = FALSE;
-                perror("End of stream, returning ...\n");
-                break;
-            case -1:
-                perror("Error reading from network!\n");
-                keep_going = FALSE;
-                break;
+    // attempt to resolve hostname
+    if( (host_struct = gethostbyname(time_server_name)) == NULL)
+        {
+        printf("failed to resolve hostname\n");
+        exit(EXIT_FAILURE);
         }
-        printf("%c", input);
-        
-        // check if we terminate
-        if (input == 'q') {
-            keep_going = FALSE;
+    
+    //attempt to resolve IP address(es)
+    if(host_struct->h_addrtype == AF_INET)
+        {
+        addr_list = (struct in_addr **)host_struct->h_addr_list;
+        for(index=0; addr_list[index] != NULL; index++)
+            {
+            strcpy(server_IP, inet_ntoa(*addr_list[index]));    
+            }
         }
-        
-        // send result back to client
-        write(client_socket, &input, sizeof(char));
+    else
+        {
+        printf("failed to resolve host IP\n");
+        exit(EXIT_FAILURE);
+        }
+
+    // create an unnamed socket, and then name it
+    time_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // create addr struct
+    time_address.sin_family = AF_INET;
+    time_address.sin_addr.s_addr = inet_addr(server_IP);
+    time_address.sin_port = htons(TIME_PORT);
+    
+    // connect to time server socket
+    if (connect(time_socket, (struct sockaddr *)&time_address, sizeof(time_address)) == -1) {
+        perror("Error connecting to server!\n");
+        exit(EXIT_FAILURE);
     }
+    
+    //read data from time server
+    index = 0;
+    while( read(time_socket, &in_char, sizeof(in_char)) != '\0' )
+        {
+        output_time[index] = in_char;
+        index++;
+        output_time[index] = '\0';
+        }
+    close(time_socket);
+    
+    // relay data to client
+    write(client_socket, &output_time, sizeof(char) * strlen(output_time));
     
     // cleanup
     if (close(client_socket) == -1) {
